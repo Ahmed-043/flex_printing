7bf5bc9756d354a18fb3362eee324374a1e3df3c
Muhammad Ahmed Ejaz <g1f23ubscs043@ucp.edu.pk>
2026-05-01 19:26:21 +0500

    home scroll to top and optimization

--- lib/models/System/system.dart
+++ lib/models/System/system.dart
@@ -1,11 +1,11 @@
 import 'system_platform_stub.dart'
 if (dart.library.html) 'system_platform_web.dart'
 if (dart.library.io) 'system_platform_io.dart';
+//
+// class System {
+//   static bool get isMobile => PlatformSystem.isMobile;
+// }
 
 class System {
-  static bool get isMobile => PlatformSystem.isMobile;
+  static bool isMobile = true;
 }
-
-// class System {
-//   static bool isMobile = true;
-// }
