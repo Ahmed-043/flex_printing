9e6638e2d9f42c114d6970be6bcfe355e16409a2
Muhammad Ahmed Ejaz <g1f23ubscs043@ucp.edu.pk>
2026-05-03 14:28:26 +0500

    optimizations

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,9 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 Future<void> main() async {
- // usePathUrlStrategy();
+  usePathUrlStrategy();
 
   WidgetsFlutterBinding.ensureInitialized();
 
