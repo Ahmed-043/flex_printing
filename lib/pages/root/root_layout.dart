7ab1f9d4bd355323875edb16c12d18088433b240
Muhammad Ahmed Ejaz <g1f23ubscs043@ucp.edu.pk>
2026-05-05 16:10:41 +0500

    optimizations

--- lib/pages/root/root_layout.dart
+++ lib/pages/root/root_layout.dart
@@ -86,6 +86,7 @@ class _RootLayoutState extends State<RootLayout> {
                   leading: InkWell(
                     hoverColor: Colors.transparent,
                     splashColor: Colors.transparent,
+                    highlightColor: Colors.transparent,
                     onTap: () => _handleHomeTap(isHome),
                     child: Row(
                       crossAxisAlignment: .center,
